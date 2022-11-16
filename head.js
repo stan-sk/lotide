d7c69993eee88ed8518c114e9eca752bf5526e8a
Stanley Lai <stanley.lai@telus.com>
2022-11-15 21:42:09 -0800

    add eqArrays

--- head.js
+++ head.js
@@ -8,8 +8,8 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
 
 const head = function(array) {
