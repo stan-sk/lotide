b58b1bea9444839e15f31fc07d5b0acf0417f838
Stanley Lai <stanley.lai@telus.com>
2022-11-16 18:15:25 -0800

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const countLetters = function (string) {
+  let result = {};
+  for (let character of string) {
+    if (character !== " ") {
+      if (!result[character]) {
+        result[character] = 1;  // left hand side is key; right hand side is value
+      } else {
+        result[character] += 1;
+      }
+    }
+  }
+  return result
+}
+
+
+console.log(countLetters("lighthouse in the house"))
+
+
+
+
+// (!result[character]) 
+//check if value exisit yet
