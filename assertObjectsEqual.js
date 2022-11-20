bfe8ec2e8b354cdf28d792570ade4c9f178909c6
Stanley Lai <stanley.lai@telus.com>
2022-11-20 11:32:38 -0800

    assertobjecteq

---
+++ assertObjectsEqual.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+   if (arr1[i] !== arr2[i]) {
+     return false;
+   }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  let result = false;
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  
+  if (keys1.length !== keys2.length) {
+    return false
+  } 
+  for (let key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(object1, object2)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
+  }
+
+};
--- without.js
+++ without.js
@@ -1,19 +1,10 @@
-
-const without = function(source, itemsToRemove) {
-  let updatedItems = [];
-  for (let i = 0; i < source.length; i++) {
-    let matches = [];
-    for (let x = 0; x < itemsToRemove.length; x++) {
-      
-      if (source[i] === itemsToRemove[x]) {
-        matches.push(source[i]);
-      }
+const without = function (source, itemToRemove) {
+  let newArr = [];
+  for (let item of source)
+    if (!itemToRemove.includes(item)) {
+      newArr.push(item)
     }
-    if (matches.indexOf(source[i]) === -1) {
-      updatedItems.push(source[i]);
-    }
-  }
-  return updatedItems;
+  return newArr;
 }
 
 
@@ -21,18 +12,33 @@ const without = function(source, itemsToRemove) {
 console.log(without([1, 2, 3], [1])); // => [2, 3]
 console.log(without(["5", "6", "7"], [1, 7, "6"]));
 
+// const without = function(source, itemsToRemove) {
+//   let updatedItems = [];
+//   for (let i = 0; i < source.length; i++) {
+//     let matches = [];
+//     for (let x = 0; x < itemsToRemove.length; x++) {
+      
+//       if (source[i] === itemsToRemove[x]) {
+//         matches.push(source[i]);
+//       }
+//     }
+//     if (matches.indexOf(source[i]) === -1) {
+//       updatedItems.push(source[i]);
+//     }
+//   }
+//   return updatedItems;
+// }
 
-/*
-const without = function (arr, item) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].toString() !== item.toString()) {
-      newArr.push(arr[i]);
-    }
-  }
-  return newArr
-}
+
+// const without = function (arr, item) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].toString() !== item.toString()) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr
+// }
 
 
-console.log(without(["1", "2", "3"], [1, 2, "3"]))
-*/
+
